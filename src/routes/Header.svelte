ed6cf5c36d5661ec7828e15e6ed963c17fdb8897
Pascal <p.bawidamann@outlook.com>
2022-11-05 18:49:04 +0100

    New Header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
+	import logo from '$lib/images/logo-removebg-preview.png';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
+		<a href="/">
 			<img src={logo} alt="SvelteKit" />
 		</a>
 	</div>
@@ -19,11 +19,11 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/Vault' ? 'page' : undefined}>
+				<a href="/vault">Vault</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/Encounter') ? 'page' : undefined}>
+				<a href="/encounter">Encounter</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -40,8 +40,11 @@
 
 <style>
 	header {
+		width: 100%;
 		display: flex;
 		justify-content: space-between;
+		background-color: black;
+		height: 4em;
 	}
 
 	.corner {

5dfe36a404b0490a0508fd2c1ad297fc4f56da4c
Pascal Bawidamann <bawidamann@itestra.de>
2022-11-07 14:08:03 +0100

    Inputs look good now

---
+++ src/routes/vault/components/EditorRowInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<div class="editor-row">
+	<label for={name}>
+		<span>{name}</span>
+		<slot />
+	</label>
+</div>
+
+<style>
+	.editor-row {
+		margin-top: 2rem;
+		width: 40rem;
+	}
+</style>
--- src/routes/vault/spells/[id]/+page.svelte
+++ src/routes/vault/spells/[id]/+page.svelte
@@ -1,45 +1,55 @@
 <script lang="ts">
+	import EditorRowInput from '../../components/EditorRowInput.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const { spell } = data;
 </script>
 
-<section>
-	<h1>Spell Editor</h1>
+<section class="px-16 py-8 flex flex-row justify-center">
+	<div>
+		<h1>Spell Editor</h1>
+		<div class="flex flex-row">
+			<div class="flex flex-col mr-4">
+				<EditorRowInput name="Name">
+					<input type="text" id="Name" bind:value={spell.name} minlength="2" required />
+				</EditorRowInput>
 
-	<div class="editor-row">
-		<label for="name">
-			<span>Name</span>
-			<input class="w-28" type="text" id="name" bind:value={spell.name} minlength="2" required />
-		</label>
-	</div>
+				<EditorRowInput name="description">
+					<textarea class="h-64" id="description" bind:value={spell.desc} minlength="2" required />
+				</EditorRowInput>
+			</div>
 
-	<div class="editor-row">
-		<div>casting time</div>
-		<div>{spell.castingTime}</div>
-	</div>
-	<div class="editor-row">
-		<div>range</div>
-		<div>{spell.range}</div>
-	</div>
-	<div class="editor-row">
-		<div>components</div>
-		<div>{spell.components}</div>
-	</div>
-	<div class="editor-row">
-		<div>duration</div>
-		<div>{spell.duration}</div>
-	</div>
-	<div class="editor-row">
-		<div>description</div>
-		<div>{spell.desc?.join('\n')}</div>
+			<div class="flex flex-col ml-4">
+				<EditorRowInput name="castingTime">
+					<input
+						type="text"
+						id="castingTime"
+						bind:value={spell.castingTime}
+						minlength="2"
+						required
+					/>
+				</EditorRowInput>
+
+				<EditorRowInput name="Range">
+					<input type="text" id="range" bind:value={spell.range} minlength="2" required />
+				</EditorRowInput>
+
+				<EditorRowInput name="Components">
+					<input type="text" id="components" bind:value={spell.components} minlength="2" required />
+				</EditorRowInput>
+
+				<EditorRowInput name="Duration">
+					<input type="text" id="duration" bind:value={spell.duration} minlength="2" required />
+				</EditorRowInput>
+			</div>
+		</div>
 	</div>
 </section>
 
 <style>
 	.editor-row {
-		display: flex;
-		flex-direction: row;
+		margin-top: 2rem;
+		width: 40rem;
 	}
 </style>
